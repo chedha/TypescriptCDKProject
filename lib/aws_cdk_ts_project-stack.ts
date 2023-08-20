ed1da0368adfba996ff669dec0858abe676958be
Chris Hall <chedha@gmail.com>
2023-08-20 12:00:19 -0400

    initial commit

--- lib/aws_cdk_ts_project-stack.ts
+++ lib/aws_cdk_ts_project-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class AwsCdkTsProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new lambda.Function(this, 'lambdaFunction', {
+      functionName: 'first-cdk-lambda',
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 128
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkTsProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
